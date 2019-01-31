f42e6626e72ec2fd1c8676237e36ef6070d1229d
yordv <yordan@ednodarvo.io>
2019-01-31 12:15:45 +0200

    add doctrine friendly name to Gist indexes

--- src/AppMain/Entity/Geometry/MultiLine.php
+++ src/AppMain/Entity/Geometry/MultiLine.php
@@ -1,20 +1,18 @@
 <?php
 
-
 namespace App\AppMain\Entity\Geometry;
 
 use Doctrine\ORM\Mapping as ORM;
 
-# indexes={@ORM\Index(columns={"coordinates"})},
 /**
  * @ORM\Table(
  *     name="multiline",
  *     schema="x_geometry",
+ *     indexes={@ORM\Index(columns={"coordinates"})},
  *     uniqueConstraints={@ORM\UniqueConstraint(name="UNIQ_B8D4B651D17F50A3", columns={"uuid"})}
  * )
  * @ORM\Entity()
  */
 class MultiLine extends AbstractGeometry
 {
-
 }
--- src/AppMain/Entity/Geometry/Point.php
+++ src/AppMain/Entity/Geometry/Point.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\AppMain\Entity\Geometry;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -9,11 +8,11 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(
  *     name="point",
  *     schema="x_geometry",
+ *     indexes={@ORM\Index(columns={"coordinates"})},*
  *     uniqueConstraints={@ORM\UniqueConstraint(name="UNIQ_B8D4B651D17F50A2", columns={"uuid"})}
  * )
  * @ORM\Entity()
  */
 class Point extends AbstractGeometry
 {
-
 }
