78fdfafb5cb95043c81c25c44a0765f0ddc6357e
yordv <yordan@ednodarvo.io>
2019-02-02 22:27:45 +0200

    prototype

--- src/AppMain/Controller/APIFrontEnd/MapController.php
+++ src/AppMain/Controller/APIFrontEnd/MapController.php
@@ -31,6 +31,62 @@ class MapController extends AbstractController
 
         $conn = $this->entityManager->getConnection();
 
+        /*
+
+        SELECT
+            g.uuid AS id,
+            st_asgeojson(m.coordinates) AS coordinates,
+            jsonb_build_object(
+                '_sct', c.name,
+                'name', g.attributes->'name',
+                'type', g.attributes->'type'
+            ) as attributes
+        FROM
+            x_geometry.geometry_base m
+                INNER JOIN
+            x_geospatial.geo_object g ON m.geo_object_id = g.id
+                INNER JOIN
+            x_survey.survey_element e ON g.object_type_id = e.object_type_id
+                INNER JOIN
+            x_survey.survey_category c ON e.category_id = c.id
+                INNER JOIN
+            x_geospatial.object_type_visibility v ON g.object_type_id = v.object_type_id
+                INNER JOIN
+            x_survey.survey s ON c.survey_id = s.id
+        WHERE
+            s.is_active = TRUE
+            AND ST_Intersects(m.coordinates, ST_MakePolygon(ST_GeomFromText('LINESTRING(23.23743581771851 42.71862621934375, 23.23743581771851 42.70672263493815, 23.271446228027347 42.70672263493815, 23.271446228027347 42.71862621934375, 23.23743581771851 42.71862621934375)', 4326))) = TRUE
+            AND 16 BETWEEN max_zoom AND min_zoom
+
+
+        ;
+
+        EXPLAIN ANALYSE SELECT
+            g.uuid AS id,
+            st_asgeojson(m.coordinates) AS coordinates,
+            jsonb_build_object(
+                '_au', 1,
+                'name', g.attributes->'name',
+                'type', g.attributes->'type'
+            ) as attributes
+        FROM
+            x_geometry.geometry_base m
+                INNER JOIN
+            x_geospatial.geo_object g ON m.geo_object_id = g.id
+                INNER JOIN
+            x_survey.survey_auxiliary_object_type a ON g.object_type_id = a.object_type_id
+                INNER JOIN
+            x_survey.survey s ON a.survey_id = s.id
+                INNER JOIN
+            x_geospatial.object_type_visibility v ON g.object_type_id = v.object_type_id
+        WHERE
+            s.is_active = TRUE
+            AND ST_Intersects(m.coordinates, ST_MakePolygon(ST_GeomFromText('LINESTRING(13.24841141700745 12.71148040100946 , 23.24841141700745 42.70850437629568 , 23.256994485855103 42.70850437629568 , 23.256994485855103 42.71148040100946 , 13.24841141700745 12.71148040100946)', 4326))) = TRUE
+            AND 16 BETWEEN max_zoom AND min_zoom
+
+
+         */
+
         if (null !== $in) {
             $stmt = $conn->prepare('
                 SELECT 
@@ -121,21 +177,17 @@ class MapController extends AbstractController
         while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
             $attributes = json_decode($row['attributes'], true);
 
-            $properties = [];
-
-            if (!empty(trim($attributes['name']))) {
-                $properties['name'] = $attributes['name'];
+            if (empty(trim($attributes['name']))) {
+                unset($attributes['name']);
             }
 
-            if (!empty(trim($attributes['type']))) {
-                $properties['type'] = $attributes['type'];
+            if (empty(trim($attributes['type']))) {
+                unset($attributes['type']);
             }
-
-            $properties['category'] = $row['category_name'];
 
             $result[] = [
                 'id'         => $row['id'],
-                'properties' => $properties,
+                'attributes' => $attributes,
                 'geometry'   => json_decode($row['geo'], true),
             ];
         }
@@ -148,3 +200,6 @@ class MapController extends AbstractController
         ]);
     }
 }
+
+
+
---
+++ src/AppMain/Entity/Survey/Survey/AuxiliaryObjectType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\AppMain\Entity\Survey\Survey;
+
+use App\AppMain\Entity\Geospatial\ObjectType;
+use App\AppMain\Entity\Traits\UUIDableTrait;
+use App\AppMain\Entity\UuidInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="survey_auxiliary_object_type", schema="x_survey")
+ * @ORM\Entity()
+ */
+class AuxiliaryObjectType implements UuidInterface
+{
+    use UUIDableTrait;
+
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\AppMain\Entity\Survey\Survey\Survey")
+     * @ORM\JoinColumn(referencedColumnName="id", name="survey_id", nullable=true)
+     */
+    private $survey;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\AppMain\Entity\Geospatial\ObjectType")
+     * @ORM\JoinColumn(referencedColumnName="id", name="object_type_id", nullable=false)
+     */
+    private $geoObjectType;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $behavior;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getSurvey(): ?Survey
+    {
+        return $this->survey;
+    }
+
+    public function setSurvey(Survey $survey): void
+    {
+        $this->survey = $survey;
+    }
+
+    public function getObjectType(): ?ObjectType
+    {
+        return $this->geoObjectType;
+    }
+
+    public function setGeoObjectType(ObjectType $objectType): void
+    {
+        $this->geoObjectType = $objectType;
+    }
+
+    public function getBehavior()
+    {
+        return $this->behavior;
+    }
+
+    public function setBehavior(string $behavior): void
+    {
+        $this->behavior = $behavior;
+    }
+}
