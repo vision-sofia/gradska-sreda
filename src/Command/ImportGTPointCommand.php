32b9b3e09797d53ac1d526c4676ac9fd779548fe
yordv <yordan@ednodarvo.io>
2019-02-04 18:20:06 +0200

    import gt and metro fixtures

---
+++ src/Command/ImportGTPointCommand.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ImportPointCommand extends Command
+{
+    protected static $defaultName = 'app:import-gt';
+
+    protected $entityManager;
+    protected $container;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ContainerInterface $container
+    ) {
+        $this->entityManager = $entityManager;
+        $this->container = $container;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $string = file_get_contents($this->container->getParameter('kernel.root_dir') . DIRECTORY_SEPARATOR . 'DataFixtures/Raw/gt.json');
+        $json_a = json_decode($string, true);
+
+        /** @var Connection $conn */
+        $conn = $this->entityManager->getConnection();
+
+        $stmt = $conn->prepare('SELECT * FROM x_geospatial.object_type');
+        $stmt->execute();
+
+        $objectTypes = [];
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $objectTypes[$row['name']] = $row['id'];
+        }
+
+        $stmt = $conn->prepare('
+            INSERT INTO x_geometry.geometry_base (
+                geo_object_id,
+                coordinates, 
+                metadata, 
+                uuid
+            ) VALUES (
+                :spatial_object_id,
+                :geography, 
+                \'{}\', 
+                :uuid                      
+            )
+        ');
+
+        $stmtSPO = $conn->prepare('
+            INSERT INTO x_geospatial.geo_object (
+                attributes,
+                uuid,
+                object_type_id,
+                name
+            ) VALUES (
+                :attr,        
+                :uuid,
+                :object_type_id,
+                :name                 
+            )
+        ');
+
+        $j = $i = 0;
+
+        foreach ($json_a as $item) {
+            if (\is_array($item)) {
+                print_r($item);
+                foreach ($item as $s) {
+
+                    if (isset($s['geometry']['rings'][0])) {
+                        $p = [];
+                        foreach ($s['geometry']['rings'][0] as $points) {
+                            $p[] = implode(' ', $points);
+                        }
+
+                        $im = implode(',', $p);
+
+                        $name = '';
+
+                        $objectTypeId = $objectTypes['Градоустройствена единица'];
+
+                        if (isset($s['attributes']['Rajon'])) {
+                            $name = $s['attributes']['Rajon'];
+                        }
+
+                        $stmtSPO->bindValue('attr', json_encode($s['attributes']));
+                        $stmtSPO->bindValue('uuid', Uuid::uuid4());
+                        $stmtSPO->bindValue('name', $name);
+                        $stmtSPO->bindValue('object_type_id', $objectTypeId);
+                        $stmtSPO->execute();
+
+                        $stmt->bindValue('spatial_object_id', $conn->lastInsertId());
+                        $stmt->bindValue('geography', 'POLYGON((' . $im . '))');
+                        $stmt->bindValue('uuid', Uuid::uuid4());
+                        $stmt->execute();
+                        ++$i;
+                    } else {
+                        ++$j;
+
+                        echo $j . ' skip' . PHP_EOL;
+                    }
+                }
+            }
+        }
+
+        echo "Done {$j}/{$i}\n";
+    }
+}
---
+++ src/Command/ImportMetroPointCommand.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Command;
+
+use App\AppMain\Entity\Geospatial\ObjectType;
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ImportGTPointCommand extends Command
+{
+    protected static $defaultName = 'app:import-gt';
+
+    protected $entityManager;
+    protected $container;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ContainerInterface $container
+    ) {
+        $this->entityManager = $entityManager;
+        $this->container = $container;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $string = file_get_contents($this->container->getParameter('kernel.root_dir') . \DIRECTORY_SEPARATOR . 'DataFixtures/Raw/gt.json');
+        $json_a = json_decode($string, true);
+
+        $objectType = $this->entityManager
+            ->getRepository(ObjectType::class)
+            ->findOneBy(['name' => 'Спирка на градски транспорт'])
+        ;
+
+        /** @var Connection $conn */
+        $conn = $this->entityManager->getConnection();
+
+        $stmt = $conn->prepare('
+            INSERT INTO x_geometry.geometry_base (
+                geo_object_id,
+                coordinates, 
+                metadata, 
+                uuid
+            ) VALUES (
+                :spatial_object_id,
+                :geography, 
+                \'{}\', 
+                :uuid                      
+            )
+        ');
+
+        $stmtSPO = $conn->prepare('
+            INSERT INTO x_geospatial.geo_object (
+                attributes,
+                uuid,
+                object_type_id,
+                name
+            ) VALUES (
+                :attr,        
+                :uuid,
+                :object_type_id,
+                :name                 
+            )
+        ');
+
+        $j = $i = 0;
+
+        foreach ($json_a as $item) {
+            if (\is_array($item)) {
+                foreach ($item as $s) {
+                    if (!isset($s['geometry']['coordinates'][0], $s['geometry']['coordinates'][1])) {
+                        ++$j;
+
+                        echo $j . ' skip' . PHP_EOL;
+
+                        continue;
+                    }
+
+                    ++$i;
+
+                    $name = '';
+
+                    $stmtSPO->bindValue('attr', json_encode($s['properties']));
+                    $stmtSPO->bindValue('uuid', Uuid::uuid4());
+                    $stmtSPO->bindValue('name', $name);
+                    $stmtSPO->bindValue('object_type_id', $objectType->getId());
+                    $stmtSPO->execute();
+
+                    $stmt->bindValue('spatial_object_id', $conn->lastInsertId());
+                    $stmt->bindValue('geography', sprintf('POINT(%s %s)', $s['geometry']['coordinates'][0], $s['geometry']['coordinates'][1]));
+                    $stmt->bindValue('uuid', Uuid::uuid4());
+                    $stmt->execute();
+                }
+            }
+        }
+
+        echo "Done {$j}/{$i}\n";
+    }
+}
