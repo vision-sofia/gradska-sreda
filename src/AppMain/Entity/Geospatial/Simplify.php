756ea08bfc0634f0969706094d5c8413dc9fb39f
yordv <yordan@ednodarvo.io>
2019-02-11 17:04:47 +0200

    simplify entity and migration

---
+++ src/AppMain/Entity/Geospatial/Simplify.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\AppMain\Entity\Geospatial;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="simplify", schema="x_geospatial")
+ * @ORM\Entity()
+ */
+class Simplify
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $tolerance;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=2)
+     */
+    private $minZoom;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=2)
+     */
+    private $maxZoom;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTolerance(): float
+    {
+        return $this->tolerance;
+    }
+
+    public function setTolerance(float $tolerance): void
+    {
+        $this->tolerance = $tolerance;
+    }
+
+    public function getMinZoom(): float
+    {
+        return $this->minZoom;
+    }
+
+    public function setMinZoom(float $minZoom): void
+    {
+        $this->minZoom = $minZoom;
+    }
+
+    public function getMaxZoom(): float
+    {
+        return $this->maxZoom;
+    }
+
+    public function setMaxZoom(float $maxZoom): void
+    {
+        $this->maxZoom = $maxZoom;
+    }
+}
---
+++ src/Migrations/Version20190211150159.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190211150159 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE x_geospatial.simplify (id INT GENERATED ALWAYS AS IDENTITY, tolerance NUMERIC(10, 2) NOT NULL, min_zoom NUMERIC(4, 2) NOT NULL, max_zoom NUMERIC(4, 2) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP TABLE x_geospatial.simplify');
+    }
+}
