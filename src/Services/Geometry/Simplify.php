220ee2c6ed2940b174e91707fa4d6ff301a419ec
yordv <yordan@ednodarvo.io>
2019-02-13 15:18:05 +0200

    simlifty service

---
+++ src/Services/Geometry/Simplify.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services\Geometry;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class Simplify
+{
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getTolerance(float $zoom): float
+    {
+
+    }
+}
