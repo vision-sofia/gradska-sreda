010c39f51d84889a30032db29733d90388707c27
yordv <yordan@ednodarvo.io>
2019-02-21 16:06:49 +0200

    settings entity and migration

---
+++ src/AppManage/Entity/Settings.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\AppManage\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="settings", schema="x_main")
+ * @ORM\Entity()
+ */
+class Settings
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $key;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function setKey(string $key): void
+    {
+        $this->key = $key;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+}
