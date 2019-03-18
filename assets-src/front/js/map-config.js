2d3ee053c9ab715d61f407f9e67e9354b75ceda0
yordv <yordan@ednodarvo.io>
2019-03-18 18:26:06 +0200

    change tileserver url

--- assets-src/front/js/map-config.js
+++ assets-src/front/js/map-config.js
@@ -1,5 +1,5 @@
 /*const mapBoxUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';*/
-const mapBoxUrl = 'https://map.dreamradio.org/styles/vs/{z}/{x}/{y}.png';
+const mapBoxUrl = 'https://gradska-sreda.dreamradio.org/map/styles/vs/{z}/{x}/{y}.png';
 const mapBoxAttribution = `&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`;
 
 export {mapBoxUrl, mapBoxAttribution}
